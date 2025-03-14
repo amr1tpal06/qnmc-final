87d9578b1128bae5fe99b720fe4a245d1e3c37d8
amr1tpal06 <contact.amritpalsandhu@gmail.com>
2025-03-14 02:48:00 +0000

    GetMintermList- renaming and removal of unused variables

--- GetMintermList.java
+++ GetMintermList.java
@@ -4,21 +4,17 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class GetMintermList {
-	static Set<String> set=new TreeSet<String>();
-	
-	@SuppressWarnings("unused")
-	private static 	String[] minList=new String[256];
-	int i = 0;
-	
+	static Set<String> MintermList=new TreeSet<>();
+
 	public void setMinList(String x){
 	
-		set.add(x);
+		MintermList.add(x);
 		
 	}
 
 
 public static Set<String> getMin(){
-	return set;
+	return MintermList;
 }
 	
 }
