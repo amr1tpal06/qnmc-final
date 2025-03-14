4e11c02029bce8cc987141ca585bea7f6f9e2f87
amr1tpal06 <contact.amritpalsandhu@gmail.com>
2025-03-14 03:48:41 +0000

    Added Singlton Design Pattern to Mintermlist Class

--- Controller.java
+++ Controller.java
@@ -5,31 +5,27 @@ import java.util.Set;
 
 public class Controller {
     private static GUI gui = null;
-    public static Set<String> mintermlist;
-    static GetMintermList minlist = new GetMintermList();
+    static GetMintermList minList = GetMintermList.getInstance();
     public static String validatedMinterm;
 
     public Controller(GUI gui) {
         this.gui=gui;
-        this.minlist=minlist;
     }
 
     public static void handleMintermInputField(JTextField mintermInputField){
         String mintext = gui.getMintermInput();
         System.out.println(mintermInputField.getText()); //get
-
         try {
             GUI.minterm = Integer.parseInt(mintext);
         } catch (NumberFormatException e) {
             GUI.minterm = -1;
         } //handle
-
         MintermValidation.validateMinterm(mintext); //return
     }
 
     public static void handleNextButton() {
         GUI.setInputField(); //set input field
-        minlist.setMinList(validatedMinterm);
+        minList.setMinList(validatedMinterm);
     }
 
     public static void handleCalculateButton() {
--- GetMintermList.java
+++ GetMintermList.java
@@ -4,18 +4,28 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class GetMintermList {
-	static Set<String> MintermList=new TreeSet<>();
+	static Set<String> MintermList;
+	private static GetMintermList instance;
 
 	public void setMinList(String x){
-	
+
 		MintermList.add(x);
-		
 	}
 
+	public static Set<String> getMin(){
+		return MintermList;
+	}
 
-public static Set<String> getMin(){
-	return MintermList;
-}
+	public static GetMintermList getInstance() {
+		if (instance == null) {
+			instance = new GetMintermList();
+		}
+		return instance;
+	}
+
+	private GetMintermList() {
+		MintermList = new TreeSet<>();
+	}
 	
 }
 
